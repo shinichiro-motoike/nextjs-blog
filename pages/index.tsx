16014e57d6f93eefe0ecd142d944b03d7712dd44
motoike <motoike.shi@gmail.com>
2022-10-23 15:30:22 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Blog</title>
+        {/* <link
+          rel="icon"
+          href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/garlic_1f9c4.png"
+        ></link> */}
+      </Head>
+      <main>Welcom!!</main>
+    </div>
+  );
+}

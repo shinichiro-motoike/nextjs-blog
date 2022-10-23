52d23fb63bcdfd8c256dbbf9792884be4af67ff1
motoike <motoike.shi@gmail.com>
2022-10-23 16:14:59 +0900

    View blog posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { GetStaticProps } from "next";
 
 export default function Home() {
   return (
@@ -14,3 +15,9 @@ export default function Home() {
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
